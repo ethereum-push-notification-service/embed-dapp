352c70871180cd60e2d1bdafe179cc8a8298e79c
Subhranshu Das <subhranshu@Subhranshus-MacBook-Pro.local>
2022-04-24 13:01:46 +0530

    updated changes for the SDK code changes

--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,15 @@
 const Helpers = {
-  createMsgPayload({ msg, ...otherParams}) {
+  createMsgPayload(payload = {}) {
     return {
-        msg,
-        channel: 'EPNS_SDK_CHANNEL', // this has to be picked from the SDK
-        ...otherParams 
+      channel: 'EPNS_SDK_CHANNEL',
+      ...payload
     };
   },
   pusblishMsgToSDK(msgPayload) {
     try {
         window.top.postMessage(JSON.stringify(msgPayload), '*');
     } catch (error) {
-        console.warn('[embed-dapp] something went wrong while passing msg to SDK');
+        console.warn('[embed-dapp] something went wrong while passing msg to SDK', error);
     }
   },
 }
