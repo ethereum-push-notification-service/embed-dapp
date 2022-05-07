7825899e0d65d82448b9a389daa62c9a3cc78ad4
Subhranshu Das <subhranshu@Subhranshus-MacBook-Pro.local>
2022-05-07 14:20:00 +0530

    fixed the chainId for Ropsten

--- src/web3/connectors.js
+++ src/web3/connectors.js
@@ -5,7 +5,7 @@ import { InjectedConnector } from '@web3-react/injected-connector'
 
 const SUPPORTED_CHAIN_IDS = [
   1, //for mainnet
-  4 ,//for ropsten
+  3 ,//for ropsten
   42, //for kovan
 ];
 
