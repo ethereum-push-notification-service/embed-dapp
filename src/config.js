abf7e50a23f39cb0cec24f50c5494695522ea342
Nilesh Gupta <guptanilesh2312@gmail.com>
2022-11-08 12:26:28 +0530

    Added polygon ids in config

--- src/config.js
+++ src/config.js
@@ -1,24 +1,21 @@
+const API_CONFIG = {
+  PROD: {
+    API_BASE_URL: "https://backend.epns.io/apis",
+    APP_LINK: "https://app.push.org/",
+    APP_ENV: "prod"
+  },
+  STAGING: {
+    API_BASE_URL: "https://backend-staging.epns.io/apis",
+    APP_LINK: "https://staging.push.org/",
+    APP_ENV: "staging"
+  }
+}
+
 const CONFIG = {
-    5: {
-      API_BASE_URL: "https://backend-staging.epns.io/apis",
-      APP_LINK: "https://staging.push.org/",
-      APP_ENV: "staging"
-    },
-    1: {
-      API_BASE_URL: "https://backend.epns.io/apis",
-      APP_LINK: "https://app.push.org/",
-      APP_ENV: "prod"
-    },
-    80001: {
-      API_BASE_URL: "https://backend-staging.epns.io/apis",
-      APP_LINK: "https://staging.push.org/",
-      APP_ENV: "staging"
-    },
-    137: {
-      API_BASE_URL: "https://backend.epns.io/apis",
-      APP_LINK: "https://app.push.org/",
-      APP_ENV: "prod"
-    }
+  1: API_CONFIG.PROD,
+  137: API_CONFIG.PROD,
+  5: API_CONFIG.STAGING,
+  80001: API_CONFIG.STAGING,
 };
 
 export default CONFIG;
